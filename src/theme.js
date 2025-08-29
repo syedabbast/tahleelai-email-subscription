9c25f41d5f61be3d6dc595fe4234bb8e9d7ea550
Syed Abbas <tahasyed110@gmail.com>
2025-08-29 15:12:06 -0400

    Add color palette to theme.js

---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const colors = {
+  oxfordBlue: "#001D3D",      // primary background
+  persianBlue: "#0F42C3",     // button/accents
+  aztecGold: "#B99F5D",       // secondary accent
+  brightGold: "#EFBF04",      // highlight
+  charcoal: "#333333",        // text
+  white: "#FFFFFF",           // backgrounds
+};
